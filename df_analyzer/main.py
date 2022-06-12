c3a26ecf208eefe121c33d2210d52f92af241910
Vinícius Alcântara <vinicius.7x@gmail.com>
2022-06-12 20:23:21 -0300

    added simple menu for file selection

--- df_analyzer/main.py
+++ df_analyzer/main.py
@@ -12,25 +12,34 @@ import matplotlib.pyplot as plt
 if not os.path.exists('histograms'):
 	os.makedirs('histograms')
 
-def import_file():
+def select_file():
 
 	"""Read .csv or .xlsx file contained in the /input directory"""
 
 	print('Please make sure your file is in the /input directory.')
 	print('If you are just trying this package, try the TEST data.')
-	name = input('Insert file name (TEST for test data): ')
+	
+	# display available files
+	files = dict(enumerate(os.listdir('input')))
 
-	if name == 'TEST':
-		name = 'input/heart_failure_clinical_records_dataset.csv'
+	print('\nFiles available in /input:\n')
+	for file in files:
+		print(f'{file}: {files[file]}')
 
-	if name[-4:] == '.csv':
+	# selection
+	file_number = int(input("\nSelect a file by index (like 0): "))
+	selected_file = files[file_number]
+	path_to_file = f'input/{selected_file}'
 
-		df = pd.read_csv(name)
+	# loading file
+	if selected_file[-4:] == '.csv':
+
+		df = pd.read_csv(path_to_file)
 		print('Great, file loaded.')
 
-	elif name [-5:] == '.xlsx':
+	elif selected_file [-5:] == '.xlsx':
 
-		df = pd.read_excel(name)
+		df = pd.read_excel(path_to_file)
 		print('Great, file loaded.')
 
 	else:
@@ -38,12 +47,16 @@ def import_file():
 		print('Only .csv or .xlsx for now. Check your file.')
 		return None
 
-	return df
+	# returning also the file name for later folder creation
+	selected_file = selected_file[:-4]
+
+	return df, selected_file
 
 def analyze_file(df):
 
 	"""Brings a summary for the file: number of rows/columns, which columns are available and your respective data types."""
 
+	print('\nSUMMARY\n')
 	print(f'Rows: {len(df)}')
 	print(f'Columns: {len(df)}')
 	print('-')
@@ -53,17 +66,23 @@ def analyze_file(df):
 	for c in df.columns:
 		print(f'{df[c].name}: {df[c].dtype}')
 
-def generate_histograms(df):
+def generate_histograms(df, selected_file):
 
 	"""Generate histograms for all numeric columns. Histograms are available in /histograms directory, named by column name."""
+
+	# creating directory for current file
+	if not os.path.exists(f'histograms/{selected_file}'):
+		os.makedirs(f'histograms/{selected_file}')
 
 	for c in df.columns:
 		if df[c].dtypes == 'float64' or 'int64':
 			plt.hist(df[c], edgecolor = 'black')
 			plt.title(c)
-			plt.savefig(f'histograms/{c}.png')
+			plt.savefig(f'histograms/{selected_file}/{c}.png')
 			plt.close()
 
-df = import_file()
-analyze_file(df)
-generate_histograms(df)
+df, selected_file = select_file()
+
+if df is not None:
+	analyze_file(df)
+	generate_histograms(df, selected_file)
